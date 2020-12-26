0079399f19cf75be5fc6d2eddbc31827bb48befb
萩原辰哉 <hagi@hagiharatatsuyanoMacBook-Pro.local>
2020-12-26 16:29:45 +0900

    update

--- Dockerfiles/ultra96v2_env/docker_delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker ps -aq | xargs docker rm
-docker images -aq | xargs docker rmi
--- submit_json_scripts/test_prediction_model/load_np.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-pred = np.load('test_prediction.npy')
-print(pred.shape)
-print(np.unique(pred))
